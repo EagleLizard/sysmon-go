27fdc66a914ee4c6584d7b1e6d5f2c6a482b51fe
eaglelizard <whitedtd@gmail.com>
2024-09-01 00:29:47 -0600

    write sync scandir command

--- src/sysmon/cmd/scandir/scandir.go
+++ src/sysmon/cmd/scandir/scandir.go
@@ -1,33 +1,16 @@
 package scandir
 
 import (
+	"fmt"
 	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
-
-	"github.com/EagleLizard/sysmon-go/src/constants"
 )
 
-func initScanDir() string {
-	// outDataDirPath := filepath.Join(constants.BaseDir(), constants.OutDataDirName)
-	// scanDirOutDirPath := filepath.Join(outDataDirPath, constants.ScanDirOutDirName)
-	scanDirOutDirPath := GetScanDirOutDirPath()
-	// os.Mkdir(outDataDirPath, 0755)
-	os.MkdirAll(scanDirOutDirPath, 0755)
-	return scanDirOutDirPath
-}
-
-func GetScanDirOutDirPath() string {
-	outDataDirPath := filepath.Join(constants.BaseDir(), constants.OutDataDirName)
-	scanDirOutDirPath := filepath.Join(outDataDirPath, constants.ScanDirOutDirName)
-	return scanDirOutDirPath
-}
-
 type ScanDirRes struct {
-	DirsFilePath, FilesFilePath string
-	FileCount, DirCount         int
+	FileCount, DirCount int
 }
 
 type ScanDirCbParams struct {
@@ -40,26 +23,10 @@ type ScanDirCbParams struct {
 type ScanDirCb func(scanDirCbParams ScanDirCbParams)
 
 func ScanDir(dir string, scanDirCb ScanDirCb) ScanDirRes {
-	scanDirOutDir := initScanDir()
-	filesFileName := "0_files.txt"
-	filesPath := filepath.Join(scanDirOutDir, filesFileName)
-	dirsFileName := "0_dirs.txt"
-	dirsPath := filepath.Join(scanDirOutDir, dirsFileName)
 
 	dirFs := os.DirFS(dir)
 	dirCount := 0
 	fileCount := 0
-
-	filesWriter, err := os.Create(filesPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer filesWriter.Close()
-	dirsWriter, err := os.Create(dirsPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer dirsWriter.Close()
 
 	fs.WalkDir(dirFs, ".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
@@ -68,21 +35,27 @@ func ScanDir(dir string, scanDirCb ScanDirCb) ScanDirRes {
 			}
 		}
 		fullPath := filepath.Join(dir, path)
+		lstats, err := os.Lstat(fullPath)
+		if err != nil {
+			fmt.Println(fmt.Errorf("path: %s", fullPath))
+			panic(err)
+		}
 		if d.IsDir() {
 			dirCount++
-			dirsWriter.Write([]byte(fullPath + "\n"))
 		} else {
 			fileCount++
-			filesWriter.Write([]byte(fullPath + "\n"))
 		}
-		// fmt.Println(path)
+		scanDirCb(ScanDirCbParams{
+			IsDir:     d.IsDir(),
+			IsSymLink: lstats.Mode()&fs.ModeSymlink != 0,
+			FullPath:  fullPath,
+			stats:     &lstats,
+		})
 		return nil
 	})
 	res := ScanDirRes{
-		DirsFilePath:  dirsPath,
-		FilesFilePath: filesPath,
-		FileCount:     fileCount,
-		DirCount:      dirCount,
+		FileCount: fileCount,
+		DirCount:  dirCount,
 	}
 	return res
 }
--- src/sysmon/cmd/scandir/scandircmd.go
+++ src/sysmon/cmd/scandir/scandircmd.go
@@ -2,19 +2,81 @@ package scandir
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
 
+	"github.com/EagleLizard/sysmon-go/src/constants"
 	"github.com/EagleLizard/sysmon-go/src/lib/argv"
 )
 
+const progressMod = 1e4
+
 func ScanDirCmd(pargv argv.ParsedArgv) {
 	fmt.Println("ScanDirCmd()")
+
+	scanDirOutDir := initScanDir()
+	filesFileName := "0_files.txt"
+	filesPath := filepath.Join(scanDirOutDir, filesFileName)
+	dirsFileName := "0_dirs.txt"
+	dirsPath := filepath.Join(scanDirOutDir, dirsFileName)
+
+	filesWriter, err := os.Create(filesPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer filesWriter.Close()
+	dirsWriter, err := os.Create(dirsPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dirsWriter.Close()
+
+	pathCount := 0
+
+	scanDirCb := func(params ScanDirCbParams) {
+		if params.IsSymLink {
+			return
+		}
+		lineBytes := []byte(fmt.Sprintf("%s\n", params.FullPath))
+		if params.IsDir {
+			dirsWriter.Write(lineBytes)
+		} else {
+			filesWriter.Write(lineBytes)
+		}
+		pathCount++
+		if pathCount%progressMod == 0 {
+			fmt.Print(".")
+		}
+	}
+
 	dirs := pargv.Args
 	fmt.Println("Scanning:")
+	startTime := time.Now()
 	for _, currDir := range dirs {
 		/*
 			TODO: make this async
 		*/
 		fmt.Printf("%s\n", currDir)
-
+		ScanDir(currDir, scanDirCb)
+		fmt.Print("\n")
 	}
+	endTime := time.Since(startTime)
+	fmt.Printf("Scan took: %s\n", endTime)
+}
+
+func initScanDir() string {
+	// outDataDirPath := filepath.Join(constants.BaseDir(), constants.OutDataDirName)
+	// scanDirOutDirPath := filepath.Join(outDataDirPath, constants.ScanDirOutDirName)
+	scanDirOutDirPath := GetScanDirOutDirPath()
+	// os.Mkdir(outDataDirPath, 0755)
+	os.MkdirAll(scanDirOutDirPath, 0755)
+	return scanDirOutDirPath
+}
+
+func GetScanDirOutDirPath() string {
+	outDataDirPath := filepath.Join(constants.BaseDir(), constants.OutDataDirName)
+	scanDirOutDirPath := filepath.Join(outDataDirPath, constants.ScanDirOutDirName)
+	return scanDirOutDirPath
 }
